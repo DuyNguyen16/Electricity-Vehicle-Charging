f32b4dac1e8d4796e3473c6edf99302e655cb484
DuyNguyen16 <khanhduyn161@gmail.com>
2024-09-29 14:13:42 +1000

    charging stations vector

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <vector>
 #include "ChargingStation.h"
+#include "Stations.h"
 
 using namespace std;
 
 int main() {
+    Stations s;
+    vector<ChargingStation> cStations = s.getCStation();
     cout << "Charging Station Information:" << endl;
-    ChargingStation station1(1);
     cout << "Location Id" << setw(20) << "Location Name"
          << setw(25) << "Distance to Sydney" << setw(20)
          << "no of Chargers" << endl;
-    station1.display();
+    
+    for (int i = 0; i < NUM_CITIES; i++) {
+        cStations[i].display();
+    }
 
     return 0;
 }
--- Stations.h
+++ Stations.h
@@ -1,6 +1,23 @@
-#ifdef STATIONS_H_
-#define STATION_H_
+#ifndef STATIONS_H_
+#define STATIONS_H_
 
+#include "ChargingStation.h"
+#include <vector>
+class Stations {
+private:
+    vector<ChargingStation> cStations;
+public:
+    vector<ChargingStation> &getCStation();
+};
+
+vector<ChargingStation> &Stations::getCStation() {
+    for (int i = 0; i < NUM_CITIES; i++) {
+        ChargingStation aStation(i);
+        cStations.push_back(aStation);
+    }
+
+    return cStations;
+}
 
 
 #endif
